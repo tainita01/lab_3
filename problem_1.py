1dbf5589d79c2dc52ffa44f4536ddff54a2598aa
tainita01 <tainita01@gmail.com>
2014-10-23 18:11:35 -0700

    Update problem_1.py

--- problem_1.py
+++ problem_1.py
@@ -1,11 +1,16 @@
 import os, sys
+
+# Opening the folder
 abspath = "F:/UWTacoma/GIS_501_AU_2014/lab_3"
 folder = abspath + '/text_files'
 files = os.listdir(folder)
+
+# Defining the variables
 basefile = ""
 filename = {}
 ext1=['.rtf','.py','.text']
 
+# Renaming with the for loop
 for filename in files:
   infilename = os.path.join(folder,filename)
   base_file, ext = os.path.splitext(filename)
